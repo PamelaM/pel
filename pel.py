fbcc50060c0c885a1780f5adc60f8edb6c1d83f3
PamelaM <pamela@mcanulty.org>
2024-06-09 20:39:00 -0400

    Get show selected and hide conflicts

--- pel.py
+++ pel.py
@@ -27,6 +27,12 @@ def import_pel_pdf():
 
 
 class Event:
+    def clean_name(self, name):
+        name = name.replace("ThemeGame", "Theme Game")
+        name = name.replace("—", " — ")
+        name = " ".join(name.split(None))
+        return name
+
     def __init__(self, lines):
         try:
             self.separators = SEPARATORS[::]
@@ -34,7 +40,7 @@ class Event:
             self.event_id, self.name = lines[0].split(None, 1)
             self.name = f"{self.name}{self.next()}".strip()
             pieces = pattern.split(self.name)
-            self.name = ''.join(pieces[:-2])
+            self.name = self.clean_name(''.join(pieces[:-2]))
             self.day, extra = pieces[-2:]
             extra = extra.strip(",").strip()
             hour, length = extra.rsplit(",", 1)
@@ -53,7 +59,7 @@ class Event:
             for idx, c in enumerate("WTFSZ"):
                 event_slot = event_slot.replace(c, str(idx))
             self.event_slot = event_slot
-            self.event_number = self.event_id.split(":")[-1].strip()
+            self.safe_event_id = self.event_id.replace(":", '-').strip()
             start_hour = int(time_slot[1:])
             day_letter = time_slot[0]
             duration = round(float(self.length.split(None)[0]) + 0.5)
@@ -61,6 +67,7 @@ class Event:
                 f"{day_letter}{(start_hour + idx):02d}"
                 for idx in range(duration)
             ]
+            self.conflicts = set()
         except Exception:
             print(len(lines), lines[0], self.text)
             raise
@@ -73,6 +80,26 @@ class Event:
             value, self.text = self.text.split(f" {sep}: ")
             val = value.strip(",").strip()
         return "" if val == "NONE" else val
+
+    def as_dict(self):
+        return dict(
+            event_id=self.safe_event_id,
+            name=self.name,
+            day=self.day,
+            hour=self.hour,
+            length=self.length,
+            players=self.players,
+            location=self.location,
+            GM=self.GM,
+            sponsor=self.sponsor,
+            prize=self.prize,
+            period=self.period,
+            scale=self.scale,
+            rules=self.rules,
+            description=self.description,
+            time_slots=self.time_slots,
+            conflicts=sorted(self.conflicts),
+        )
 
 
 def read_pel_lines():
@@ -112,15 +139,10 @@ def pel_text_to_events():
 
 
 def event_to_table_row(evt):
-    name = evt.name.replace("ThemeGame", "Theme Game")
-    name = name.replace("—", " — ")
-    name = " ".join(name.split(None))
-    name = name.replace("— Theme Game", "<br/><em>— Theme Game</em>")
-    checkbox = f"<input type='checkbox' class='chk' id='chk-state-{evt.event_number}'/>"
     data = [
-        checkbox,
+        f"<input type='checkbox' class='chk' id='chk-state-{evt.safe_event_id}'/>",
         "<br/>".join([evt.event_slot, evt.event_id, evt.day, evt.hour]),
-        name,
+        evt.name.replace("— Theme Game", "<br/><em>— Theme Game</em>"),
         evt.length,
         evt.players,
         evt.location.replace(":", "<br/>"),
@@ -132,62 +154,50 @@ def event_to_table_row(evt):
         evt.sponsor,
         evt.prize,
         'PELNOTSELECTED',
-        ' '.join(evt.time_slots),
+        'PELNOCONFLICTS',
     ]
-    cells = [
-        f"<td>{v}</td>"
-        for v in data
-    ]
-    cells[-2] = cells[-2].replace("<td>", f"<td id='hidden-{evt.event_number}'>")
-    cells[-1] = cells[-1].replace("<td>", f"<td id='time-slots-{evt.event_number}'>")
+    cells = [f"<td>{v}</td>" for v in data]
+
+    cells[-2] = cells[-2].replace("<td>", f"<td id='hidden-selected-{evt.safe_event_id}'>")
+    cells[-1] = cells[-1].replace("<td>", f"<td id='hidden-conflicts-{evt.safe_event_id}'>")
     row = "".join(cells)
-    return f"<tr id='evt-{evt.event_number}'>{row}</tr>"
+    return f"<tr id='evt-{evt.safe_event_id}'>{row}</tr>"
 
 
 def create_context():
     ctx = {}
+
     column_names = [
         "", "ID/Time", "Name", "Length", "Players", "Location",
         "GM", "Period", "Scale", "Rules", "Description", "Sponsor", "Prize",
-        "hidden-selected", "hidden-time-slots"
+        "hidden-selected", "hidden-conflicts"
     ]
-    hidden_cols = ["Location", 'Sponsor', 'Prize', 'hidden-selected', 'hidden-time-slots']
 
-    ctx['table_header'] = "".join(
-        f"<th>{v}</th>"
-        for v in column_names
-    )
+    ctx['table_header'] = "".join(f"<th>{v}</th>" for v in column_names)
+
+    hidden_cols = ["Location", 'Sponsor', 'Prize', 'hidden-selected', 'hidden-conflicts']
+    ctx['hidden_selected_col_num'] = len(column_names) - 2
+    ctx['hidden_conflicts_col_num'] = len(column_names) - 1
 
     def active_class(col_name):
-        if col_name in hidden_cols:
-            return ""
-        else:
-            return "active"
+        return "" if col_name in hidden_cols else "active"
 
     column_toggles = [
-        f'<button type="button" class="btn btn-outline-secondary btn-sm toggle-vis {active_class(v)}" data-column="{idx}" aria-pressed="true">{v}</button>'
+        f'<button type="button" class="btn btn-outline-secondary btn-sm toggle-vis {active_class(v)}" autocomplete="off" data-toggle="button" data-column="{idx}" aria-pressed="false">{v}</button>'
         for idx, v in enumerate(column_names[1:-2], 1)
     ]
-    ctx['column_toggles'] = " ".join(column_toggles)
-
-    table_rows = [event_to_table_row(e) for e in pel_text_to_events()]
-    ctx['table_rows'] = "\n".join(table_rows)
+    ctx['column_toggles'] = "\n".join(column_toggles)
 
     column_defs = [
         {
             'visible': bool(col not in hidden_cols),
             'targets': idx,
-            'name_': col,
-            'searchable': True,
+            'name_': 'checkbox' if idx == 0 else col,
+            'searchable': idx != 0,
+            'orderable': (idx == 0) or col.startswith('hidden-')
         }
         for idx, col in enumerate(column_names)
     ]
-    column_defs[0]['orderable'] = False
-    column_defs[0]['searchable'] = False
-    column_defs[0]['name_'] = 'checkbox'
-
-    column_defs[-2]['orderable'] = False
-    column_defs[-1]['orderable'] = False
 
     ctx['column_defs'] = json.dumps(
         column_defs,
@@ -195,7 +205,22 @@ def create_context():
         indent=4,
     )
 
-    ctx['TIME_SLOTS'] = json.dumps(list(TIME_SLOTS))
+    # -- Events
+    events = list(pel_text_to_events())
+    time_slots = defaultdict(list)
+    for e in events:
+        for s in e.time_slots:
+            time_slots[s].append(e)
+    for evt in events:
+        for s in evt.time_slots:
+            for e in time_slots[s]:
+                evt.conflicts.add(e.safe_event_id)
+
+    edict = {e.safe_event_id: e.as_dict() for e in events}
+    ctx['events'] = json.dumps(edict, sort_keys=True, indent=4)
+
+    table_rows = [event_to_table_row(e) for e in events]
+    ctx['table_rows'] = "\n".join(table_rows)
     return ctx
 
 
@@ -208,6 +233,33 @@ def write_pel_html():
 
     with open("pel.html", "w") as f:
         f.write(html_text)
+
+
+def test():
+    events = list(pel_text_to_events())
+    edict = {e.event_id: e for e in events}
+    conflicts = {e.event_id: False for e in events}
+    time_slots = defaultdict(list)
+    for e in events:
+        for s in e.time_slots:
+            time_slots[s].append(e)
+
+    selected_evts = [edict['T09:447']]
+
+    for evt in selected_evts:
+        for s in evt.time_slots:
+            for e in time_slots[s]:
+                conflicts[e.event_id] = True
+    for evt in selected_evts:
+        conflicts[evt.event_id] = False
+
+    evt_conflicts = [
+        (evt_id, edict[evt_id])
+        for evt_id, is_conflict in sorted(conflicts.items())
+        if is_conflict
+    ]
+    for _, e in sorted(evt_conflicts):
+        print(e.event_id, e.time_slots)
 
 
 if __name__ == "__main__":
